fb0f46ec42ae9317c7cf3b467bb20d219d3a0361
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-03-28 13:40:40 +0300

    add colors and dark mode moon icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,30 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          700: "#050505",
+          600: "#1F1F1F",
+          500: "#2D2D2D",
+          400: "#3A3A3A",
+          300: "#757575",
+          200: "#E9E9E9",
+          100: "#F4F4F4",
+        },
+        red: {
+          DEFAULT: "#FF5252",
+        },
+        purple: {
+          DEFAULT: "#A445ED",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
